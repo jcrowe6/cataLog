185d7ad0b5d1c72c8964aee50c7639043774b951
Jeremiah Crowell <jcrowe6@illinois.edu>
2020-05-28 15:09:35 -0500

    cmake files configured

---
+++ include/mylibrary/example.h
@@ -0,0 +1,12 @@
+//
+// Created by jeremiah on 5/28/2020.
+//
+#ifndef CATALOG_MYLIBRARY_H
+#define CATALOG_MYLIBRARY_H
+
+namespace mylibrary {
+
+}  // namespace mylibrary
+
+
+#endif // CATALOG_MYLIBRARY_H
---
+++ src/example.cpp
@@ -0,0 +1,8 @@
+//
+// Created by jeremiah on 5/28/2020.
+//
+#include <mylibrary/example.h>
+
+namespace mylibrary {
+
+}  // namespace mylibrary
